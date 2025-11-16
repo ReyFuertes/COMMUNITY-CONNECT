2d7dac792b5b95d4fa883b6220a805a7c59944b0
Rey Fuertes <reynelfuertes@gmail.com>
2025-11-16 16:36:09 +0800

    feat: Move .gitignore files to root and consolidate

---
+++ Gateway/Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//app.UseHttpsRedirection();
+
+app.MapReverseProxy();
+
+app.Run();
+
